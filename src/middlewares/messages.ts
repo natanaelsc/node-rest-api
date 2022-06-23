6a3fae77c76c5774578e8f43e627bb45c562132c
Natanael Silva <53065432+natanaelsc96@users.noreply.github.com>
2022-06-23 18:20:43 -0300

    feat: update response messages

--- src/middlewares/messages.ts
+++ src/middlewares/messages.ts
@@ -1,13 +1,11 @@
-class Messages {
-    ok = (): string => "ok";
-    success = (): string => "success";
+export default class Messages {
+    ok = (): string => "OK";
+    success = (): string => "Success";
     welcomeMessage = (): string => "Welcome to Home Page";
     pageNotFound = (): string => "Page Not Found";
-    created = (): string => "created";
-    updated = (): string => "updated";
-    deleted = (): string => "deleted";
-    error = (): string => "error";
-    notFound = (): string => "not found";
+    created = (): string => "Created";
+    updated = (): string => "Updated";
+    deleted = (): string => "Deleted";
+    error = (): string => "Error";
+    notFound = (): string => "Not found";
 }
-
-export default Messages;
