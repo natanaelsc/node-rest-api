17fdaf09fc22d54f3e94d594517b2d38b174b640
Natanael Silva <53065432+natanaelsc96@users.noreply.github.com>
2022-06-23 18:20:25 -0300

    feat: update response codes

--- src/middlewares/codes.ts
+++ src/middlewares/codes.ts
@@ -1,9 +1,7 @@
-class Codes {
+export default class Codes {
     ok = (): number => 200;
     created = (): number => 201;
     conflict = (): number => 409;
     error = (): number => 400;
     notFound = (): number => 404;
 }
-
-export default Codes;
